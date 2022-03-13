518c65e6614a9e7d7bd71dbd71811adfd67fa182
slightly-null <adamchan@0x11.email>
2022-03-13 20:22:11 +0800

    update

--- src/logic/placeOrder.ts
+++ src/logic/placeOrder.ts
@@ -2,11 +2,12 @@ import axios from 'axios'
 import { ApiHref } from '@/const'
 const axiosIns = axios.create({
   // baseURL: ApiHref
+  withCredentials: true
 })
 
 export const SubmitOrder = async function(data) {
   return axiosIns({
-    url: '/api/v1/order/create',
+    url: 'http://api-tron-v1.dpay.systems/api/v1/order/create',
     method: 'post',
     data,
   }).then(res => res.data)
