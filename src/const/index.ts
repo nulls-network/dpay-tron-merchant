49e9ab02a9c67f235aec3276a224aecb7a8d123c
slightly-null <adamchan@0x11.email>
2022-03-13 19:20:07 +0800

    update

--- src/const/index.ts
+++ src/const/index.ts
@@ -2,8 +2,8 @@
 // export const ApiHref = 'http://192.168.10.3:7002'
 export const ApiHref = 'http://13.213.31.141'
 
-// export const ChecksSiteHref = 'https://dpay-tron-checks.vercel.app'
-export const ChecksSiteHref = 'http://192.168.10.22:4001'
+export const ChecksSiteHref = 'https://dpay-tron-checks.vercel.app'
+// export const ChecksSiteHref = 'http://192.168.10.4:3334'
 
 export const USDT = 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t' // mainnet
 // export const USDT = 'TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj' // testnet
