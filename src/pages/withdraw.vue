83b7e97bfb1185da566b03de6e23ce064b064d60
hanjiangyanhuo <hjpyh@foxmail.com>
2022-08-05 19:18:59 +0800

    update

--- src/pages/withdraw.vue
+++ src/pages/withdraw.vue
@@ -56,6 +56,14 @@
             <el-input v-model="ruleForm.from"></el-input>
         </el-form-item>
 
+        <el-form-item label="isPayment" prop="isPayment">
+            <el-switch
+                v-model="ruleForm.isPayment"
+                active-color="#13ce66"
+                inactive-color="#ff4949">
+            </el-switch>
+        </el-form-item>
+
         <el-form-item>
             <el-button type="primary" @click="submitForm(ruleFormRef)">{{ $t('common.confirm') }}</el-button>
             <el-button @click="resetForm(ruleFormRef)">{{ $t('orderTool.reset') }}</el-button>
@@ -88,6 +96,7 @@ const ruleForm = reactive({
     uuid: ``,
     signature: ``,
     from: ``,
+    isPayment: undefined
 })
 
 const response = ref('')
